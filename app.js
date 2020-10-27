2cc18977f04a490e38288f0a8e20f68f4dc91bee
ifer-itcast <yangkangkang@itcast.cn>
2020-10-27 21:21:35 +0800

    创建用户路由模块

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
+const userRouter = require('./router/user');
+app.use('/api', userRouter);
+
 app.listen(3007, () => console.log('api server running at http://127.0.0.1:3007'));
