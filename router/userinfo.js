266f0463dc622f40c83663d03a4ad818e2d0393e
ifer-itcast <yangkangkang@itcast.cn>
2020-10-28 01:24:25 +0800

    修改用户信息

--- router/userinfo.js
+++ router/userinfo.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const expressJoi = require('@escook/express-joi');
 const router = express.Router();
 
+const { update_userinfo_schema } = require('../schema/user');
 const userinfo_handler = require('../router_handler/userinfo');
+// 获取用户信息
 router.get('/userinfo', userinfo_handler.getUserInfo);
+
+// 更新用户信息
+router.post('/userinfo', expressJoi(update_userinfo_schema), userinfo_handler.updateUserInfo);
 
 module.exports = router;
--- router_handler/userinfo.js
+++ router_handler/userinfo.js
@@ -1,5 +1,6 @@
 const db = require('../db');
 
+// 获取用户信息
 exports.getUserInfo = (req, res) => {
     const sql = `select id, username, nickname, email, user_pic from ev_users where id=?`;
 
@@ -13,5 +14,17 @@ exports.getUserInfo = (req, res) => {
             message: '获取用户基本信息成功！',
             data: results[0]
         });
+    });
+};
+
+// 更新用户信息
+exports.updateUserInfo = (req, res) => {
+    const sql = `update ev_users set ? where id=?`;
+
+    db.query(sql, [req.body, req.body.id], (err, results) => {
+        if (err) return res.cc(err);
+        if (results.affectedRows !== 1) return res.cc('修改用户信息失败！');
+
+        res.cc('修改用户信息成功', 0);
     });
 };
