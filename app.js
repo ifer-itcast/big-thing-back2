4da6b0ad3d0e3d3c7eab9ed1ef863b5eb6073597
ifer-itcast <yangkangkang@itcast.cn>
2020-10-27 21:00:31 +0800

    初始化项目

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(3007, () => console.log('api server running at http://127.0.0.1:3007'));
