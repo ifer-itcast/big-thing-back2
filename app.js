3dae63402f15ea96e22fecd7c53c4723f69337d5
ifer-itcast <yangkangkang@itcast.cn>
2020-10-27 22:33:03 +0800

    封装res.cc函数

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
+
+// 统一响应
+app.use((req, res, next) => {
+    res.cc = function (err, status = 1) {
+        res.send({
+            status,
+            message: err instanceof Error ? err.message : err
+        });
+    };
+    next();
+});
 
 const userRouter = require('./router/user');
 app.use('/api', userRouter);
--- router_handler/user.js
+++ router_handler/user.js
@@ -6,26 +6,29 @@ exports.regUser = (req, res) => {
     const userinfo = req.body;
     // 1. 检测是否为空
     if (!userinfo.username || !userinfo.password) {
-        return res.send({
+        /* return res.send({
             status: 1,
             message: '用户名或密码不能为空！'
-        });
+        }); */
+        return res.cc('用户名或密码不能为空！');
     }
 
     // 2. 检测用户名是否被占用
     const sql = `select * from ev_users where username=?`;
     db.query(sql, [userinfo.username], (err, results) => {
         if (err) {
-            return res.send({
+            /* return res.send({
                 status: 1,
                 message: err.message
-            });
+            }); */
+            return res.cc(err.message);
         }
         if (results.length > 0) {
-            return res.send({
+            /* return res.send({
                 status: 1,
                 message: '用户名被占用，请更换其他用户名！'
-            });
+            }); */
+            return res.cc('用户名被占用，请更换其他用户名！');
         }
         // 3. 加密
         userinfo.password = bcrypt.hashSync(userinfo.password, 10);
@@ -36,21 +39,24 @@ exports.regUser = (req, res) => {
             password: userinfo.password
         }, (err, results) => {
             if (err) {
-                return res.send({
+                /* return res.send({
                     status: 1,
                     message: err.message
-                });
+                }); */
+                return res.cc(err.message);
             }
             if (results.affectedRows !== 1) {
-                return res.send({
+                /* return res.send({
                     status: 1,
                     message: '注册用户失败，请稍后重试！'
-                });
+                }); */
+                return res.cc('注册用户失败，请稍后重试！');
             }
-            res.send({
+            /* res.send({
                 status: 0,
                 message: '注册成功！'
-            });
+            }); */
+            res.cc('注册成功', 0);
         });
     });
 };
