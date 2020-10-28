ab44a630db2a3e1bba0b9941ee151cb9affd4285
ifer-itcast <yangkangkang@itcast.cn>
2020-10-28 19:12:05 +0800

    新增文章分类

--- router/artcate.js
+++ router/artcate.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const expressJoi = require('@escook/express-joi');
 const router = express.Router();
 
-const artcate_handler = require('../router_handler/artcate')
+const artcate_handler = require('../router_handler/artcate');
+const { add_cate_schema } = require('../schema/artcate');
 
+// 获取文章分类
 router.get('/cates', artcate_handler.getArticleCates);
+
+// 新增文章分类
+router.post('/addcates', expressJoi(add_cate_schema), artcate_handler.addArticleCates);
 
 module.exports = router;
--- router_handler/artcate.js
+++ router_handler/artcate.js
@@ -1,5 +1,6 @@
 const db = require('../db');
 
+// 获取文章分类
 exports.getArticleCates = (req, res) => {
     const sql = `select * from ev_article_cate where is_delete=0 order by id asc`;
     db.query(sql, (err, results) => {
@@ -8,6 +9,33 @@ exports.getArticleCates = (req, res) => {
             status: 0,
             message: '获取文章分类列表成功',
             data: results
+        });
+    });
+};
+
+// 新增文章分类
+exports.addArticleCates = (req, res) => {
+    const sql = `select * from ev_article_cate where name=? or alias=?`;
+    db.query(sql, [req.body.name, req.body.alias], (err, results) => {
+        if (err) return res.cc(err);
+
+        // name 被一条数据占用了，alias 被另一条数据占用了
+        if (results.length === 2) return res.cc('分类名称与别名被占用，请更换后重试！')
+
+        // name 和 alias 被一条数据完全占用了
+        if (results.length === 1 && results[0].name === req.body.name && results[0].alias === req.body.alias) return res.cc('分类名称与别名被占用，请更换后重试！');
+
+        // 分类名称被占用
+        if (results.length === 1 && results[0].name === req.body.name) return res.cc('分类名称被占用，请更换后重试！');
+
+        // 分类别名被占用
+        if (results.length === 1 && results[0].alias === req.body.alias) return res.cc('分类别名被占用，请更换后重试！');
+
+        const sql = `insert into ev_article_cate set ?`;
+        db.query(sql, req.body, (err, results) => {
+            if (err) return res.cc(err);
+            if (results.affectedRows !== 1) return res.cc('新增文章分类失败！');
+            res.cc('新增文章分类成功！', 0);
         });
     });
 };
